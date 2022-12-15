3c55c2f50309fc87d47b8f889f926dbc8f05e83e
Sébastien Barbier <contact@sebastienbarbier.com>
2022-12-15 23:18:28 +0700

    Homepage with basic layout

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,8 @@ import styles from './layout.module.scss'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
+import Typography from '@mui/material/Typography';
+
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -10,6 +12,10 @@ import IconButton from '@mui/material/IconButton';
 
 import Divider from '@mui/material/Divider';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
 
 const name = 'Seven23'
 export const siteTitle = 'Seven23 - Blog'
@@ -41,21 +47,54 @@ export default function Layout({ children, home }) {
       <main className="main">
         {children}
       </main>
-      <footer>
+      <footer id="footer" className={styles.footer}>
         <Container>
-          <div>
-              <ul>
-                  <li><a href="/">Home</a></li>
-                  <li><a target="_blank" href="https://github.com/sebastienbarbier/seven23">Github</a></li>
-                  <li><a target="_blank" href="https://seven23.readthedocs.io/en/latest/">Documentation</a></li>
-                  <li><a target="_blank" href="https://blog.seven23.io/">Blog</a></li>
-                  <li><a target="_blank" href="https://status.seven23.io/">Status</a></li>
-                  <li><a target="_blank" href="https://twitter.com/seven23_app">Twitter</a></li>
-                  <li><a target="_blank" href="https://github.com/sebastienbarbier/seven23_webapp/issues">Issue tracker</a></li>
-                  <li><a href="/legals/">Legals</a></li>
-              </ul>
-          </div>
-          <p>Developed by<br /><a href="https://sebastienbarbier.com" target="_blank">sebastienbarbier</a></p>
+          <Stack direction="row" justifyContent="space-between">
+            <nav>
+              <List>
+                <ListItem>
+                  <Stack direction="column" spacing={0}>
+                    <ListItemButton dense={true} component="a" href="/">Home</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="/product">Product</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="/pricing">Pricing</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="/resources">Resources</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="/about-us">About us</ListItemButton>
+                  </Stack>
+                </ListItem>
+              </List>
+            </nav>
+            <nav>
+              <List>
+                <ListItem>
+                  <Stack direction="column" spacing={0}>
+                    <ListItemButton dense={true} component="a" href="https://github.com/sebastienbarbier/seven23">Github</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="https://seven23.readthedocs.io/en/latest/">Documentation</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="https://blog.seven23.io/">Blog</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="https://status.seven23.io/">Status</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="https://twitter.com/seven23_app">Twitter</ListItemButton>
+                  </Stack>
+                </ListItem>
+              </List>
+            </nav>
+            <nav>
+              <List>
+                <ListItem>
+                  <Stack direction="column" spacing={0}>
+                    <ListItemButton dense={true} component="a" href="https://github.com/sebastienbarbier/seven23_webapp">Source code</ListItemButton>
+                    <ListItemButton dense={true} component="a" href="https://github.com/sebastienbarbier/seven23_webapp/issues">Issue tracker</ListItemButton>
+                  </Stack>
+                </ListItem>
+              </List>
+            </nav>
+            <Stack justifyContent="flex-end">
+              <p style={{ fontSize: '0.9em', textAlign: 'right', fontStyle: 'italic', fontWeight: 300 }}>Developed by<br />
+                <a href="https://sebastienbarbier.com">
+                  <img src="/images/svg/sebastienbarbier_logo.svg" alt="sebastienbarbier logo" style={{ height: '1.4em' }} />
+                </a>
+              </p>
+            </Stack>
+          </Stack>
+          <p style={{ textAlign: 'center', fontSize: '0.9em', paddingTop: 4, paddingBottom: 4 }}>2022 - Seven23 - <a href="/legals/">Terms of Service and Privacy Policy</a></p>
         </Container>
       </footer>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Stack from '@mui/material/Stack';
 import Layout, { siteTitle } from '../components/layout'
 import Date from '../components/date'
 
-import { getTermsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
 import Container from '@mui/material/Container';
@@ -16,20 +15,17 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 
+import EnhancedEncryptionIcon from '@mui/icons-material/EnhancedEncryption';
+import DevicesIcon from '@mui/icons-material/Devices';
+import SummarizeIcon from '@mui/icons-material/Summarize';
+
 import Avatar from '@mui/material/Avatar';
 
-export async function getStaticProps() {
-  const terms = await getTermsData()
-  return {
-    props: {
-      terms
-    }
-  }
-}
+import theme from '../components/theme';
 
 const PHOTOS = []
 
-export default function Home({ terms }) {
+export default function Home() {
   return (
     <Layout home>
       <Container>
@@ -38,7 +34,7 @@ export default function Home({ terms }) {
             <div id="title_text">
               <Stack direction="column" alignItems="flex-start" justifyContent="flex-start" spacing={2}>
                 <Typography variant="h5" gutterBottom>Fully manual budget app to track personal expenses.<br />Completely <strong>opensource</strong>, with <strong>privacy by design</strong>.</Typography>
-                <Button variant="contained" disableElevation>Launch app</Button>
+                <Button href="https://app.seven23.io" variant="contained" disableElevation>Launch app</Button>
                 <Typography variant="subtitle2" gutterBottom>Free Trial — No Credit Card Required</Typography> 
               </Stack>
             </div>
@@ -49,14 +45,35 @@ export default function Home({ terms }) {
             </aside>
           </Stack>
         </section>
-        <section>
-        </section>
-        <section>
         {/* Responsive web application with offline capacity */}
-        </section>
-        <Stack spacing={12}>
-          <Grid container spacing={2} justifyContent="center" >
-            <Grid item xs={12} md={10}>
+        <Stack spacing={18}>
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              <p>Monthly based transactions</p>
+              Categorize your transactions and review as a monthly timeline to manage your budget. You can then review them per category to help understand where your money goes
+            </Grid>
+            <Grid item xs={6}>
+              <p>Multi currency</p>
+              Each transaction is registered using its original currency, then based on a list of past exchanges the app automatically convert them in the currency of your choice. You can switch currency as you wish.
+            </Grid>
+            <Grid item xs={4}>
+              <EnhancedEncryptionIcon sx={{ fontSize: 60 }} />
+              <p>End-to-end encryption with an opensource code base to guaranty confidentiality of your data.</p>
+            </Grid>
+            <Grid item xs={4}>
+              <DevicesIcon sx={{ fontSize: 60 }} />
+              <p>Responsive web application available on phone, tablet, or desktop. Can also be used offline then synced.</p>
+            </Grid>
+            <Grid item xs={4}>
+              <SummarizeIcon sx={{ fontSize: 60 }} />
+              <p>Browse and analyse your data to generate a report and compare different budgets.</p>
+            </Grid>
+            <Grid item xs={12} container direction="column" justifyContent="center" alignItems="center">
+              <Button href="/product" style={{float: 'right' }} variant="outlined" disableElevation>See all features</Button>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid item xs={10}>
               <Stack direction="row" alignItems="center" justifyContent="center" spacing={4}>
                 <Avatar
                   alt="Sébastien Barbier"
@@ -66,15 +83,17 @@ export default function Home({ terms }) {
                 <div>
                   <p><FormatQuoteIcon fontSize="large" style={{ float: 'left', marginRight: 10 }} /> I started this project in April 2011 as I received a 723 euros salary which required me to track all expenses closly. It started as a side project, and  became a daily help on my financial tracking. This project could also help  friendsm and so time was invested to make it a saas version. I still use it  daily, and don't have plan to stop anytime soon. Expect it to last for ever.</p>
                   <Typography variant="body2" display="block" gutterBottom>Sébastien Barbier, Founder.</Typography>
-                  <Button style={{float: 'right' }} variant="outlined" disableElevation>More about us</Button>
                 </div>
+              </Stack>
+              <Stack direction="row" alignItems="center" justifyContent="center" style={{ marginTop: theme.spacing(4) }}>
+                <Button variant="outlined" disableElevation>More about us</Button>
               </Stack>
             </Grid>
           </Grid>
-          <Stack direcion="column" justifyContent="center" alignItems="center" spacing={2}>
+          <Stack direcion="column" justifyContent="center" alignItems="center" spacing={2} style={{ paddingBottom: theme.spacing(12) }}>
             <h2 style={{ marginBottom: 8 }}>We made it very simple for you to try it.</h2>
             <p style={{ textAlign: 'center' }}>Our app is very easy to try out - you can use it directly in your browser without needing to create an account.<br/>If you like it, you can at any time sign up, select a subscription plan, and start syncing your data.</p>
-            <Button variant="contained" disableElevation>Try it now</Button>
+            <Button href="https://app.seven23.io" variant="contained" disableElevation>Try it now</Button>
           </Stack>
         </Stack>
       </Container>

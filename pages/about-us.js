cd04a8d4bb5dc36e6b60b4e71da329cc297552de
Sébastien Barbier <contact@sebastienbarbier.com>
2023-01-02 21:36:05 +0100

    Improve about us page with links

--- pages/about-us.js
+++ pages/about-us.js
@@ -13,31 +13,59 @@ import utilStyles from '../styles/utils.module.css'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
 
 import Avatar from '@mui/material/Avatar'
+
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import HttpIcon from '@mui/icons-material/Http';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function AboutUs() {
   return (
     <Layout>
       <Container>
+
+
         <h2>About us</h2>
-        <p>Seven23 is a one person project, open source and free to use under a permissive MIT licence. Our goal for the future is to keep it simple and running without issues.</p>
-        
-        <h3>Team</h3>
 
         <Grid container spacing={4}>
-          <Grid item xs={12} md={6}>
-            <Stack>
-              <Paper variant="outlined" style={{ padding: '18px 26px'}}>
-                <Stack direction="row" spacing={4} alignItems="center">
-                  <Avatar
-                    alt="Sébastien Barbier"
-                    src="/images/avatar/sebastienbarbier_profile_256.jpg"
-                    sx={{ width: 80, height: 80 }}
-                  />
-                  <p style={{ fontSize: 18, fontWeight: 500 }}>Sébastien Barbier</p>
+          <Grid item xs={12} md={4} style={{ paddingRight: 20 }}>
+            <p>This project is not yet sustainable and fully maintained by volonteers. It is <strong>open source</strong>, and free to use under a permissive <strong>MIT licence</strong>.</p>
+          </Grid>
+          <Divider orientation="vertical" flexItem />
+          <Grid item xs={12} md={7}>
+            <Stack spacing={4}>
+              <Stack direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 12, sm: 4 }} alignItems={{ xs: 'flex-start', sm: 'center' }} >
+                <Avatar
+                  alt="Sébastien Barbier"
+                  src="/images/avatar/sebastienbarbier_profile_256.jpg"
+                  sx={{ width: 160, height: 160 }}
+                  variant="rounded"
+                />
+                <Stack direction="column" spacing={0}>
+                  <Typography variant="h6">Sébastien Barbier</Typography>
+                  <Typography variant="subtitle2">Cofounder</Typography>
+                  <p>Sébastien is a Cloud Software Architect from France currently based in Zurich.</p>
+                  <Stack direction="row" spacing={1}>
+                    <IconButton href="https://www.linkedin.com/in/sebbarbier/">
+                      <LinkedInIcon />
+                    </IconButton>
+                    <IconButton href="https://twitter.com/SebBarbier">
+                      <TwitterIcon />
+                    </IconButton>
+                    <IconButton href="https://github.com/sebastienbarbier/">
+                      <GitHubIcon />
+                    </IconButton>
+                    <IconButton href="https://sebastienbarbier.com">
+                      <HttpIcon />
+                    </IconButton>
+                  </Stack>
                 </Stack>
-              </Paper>
+              </Stack>
             </Stack>  
           </Grid>
         </Grid>
